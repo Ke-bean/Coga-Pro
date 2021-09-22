fb789026934ca2f997b97b1f6c82e135bdbc5c5d
Ke-bean <77304295+Ke-bean@users.noreply.github.com>
2021-09-22 02:03:07 +0200

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,154 @@
+mainData = [
+    { 
+      id: 1,
+      type: "Photos",
+      src: './Coga img/work 1.jpg',
+      tum: './Coga img/work 1.jpg',
+      // cp: "photo 1"
+    },
+    { 
+      id: 2,
+      type: "Photos",
+      src: './Coga img/work 2.jpg',
+      tum: './Coga img/work 2.jpg',
+      // cp: "photo 2"
+    },
+    { 
+      id: 3,
+      type: "Photos",
+      src: './Coga img/work 3.jpg',
+      tum: './Coga img/work 3.jpg',
+      // cp: "photo 3"
+    },
+    { 
+      id: 4,
+      type: "Design",
+      src: './Coga img/new.jpg',
+      tum: './Coga img/new.jpg',
+      // cp: "photo 4"
+    },
+    // { 
+    //   id: 5,
+    //   type: "Photos",
+    //   src: 'img/gallery/5.jpg',
+    //   tum: 'img/gallery/5.jpg',
+    // //   cp: "photo 5"
+    // },
+    // { 
+    //   id: 6,
+    //   type: "Photos",
+    //   src: 'img/gallery/6.jpg',
+    //   tum: 'img/gallery/6.jpg',
+    // //   cp: "photo 6"
+    // },
+    // { 
+    //   id: 7,
+    //   type: "Design",
+    //   src: 'img/gallery/7.jpg',
+    //   tum: 'img/gallery/7.jpg',
+    // //   cp: "photo 7"
+    // },
+    // { 
+    //   id: 8,
+    //   type: "Photos",
+    //   src: 'img/gallery/8.jpg',
+    //   tum: 'img/gallery/8.jpg',
+    // //   cp: "photo 8"
+    // },
+    // { 
+    //   id: 9,
+    //   type: "Photos",
+    //   src: 'img/gallery/9.jpg',
+    //   tum: 'img/gallery/9.jpg',
+    // //   cp: "photo 9"
+    // },{ 
+    //   id: 10,
+    //   type: "Design",
+    //   src: 'img/gallery/10.jpg',
+    //   tum: 'img/gallery/10.jpg',
+    // //   cp: "photo 10"
+    // },{ 
+    //   id: 11,
+    //   type: "Design",
+    //   src: 'img/gallery/11.jpg',
+    //   tum: 'img/gallery/11.jpg',
+    // //   cp: "photo 11"
+    // },
+    // { 
+    //   id: 12,
+    //   type: "Photoshop",
+    //   src: 'img/gallery/12.jpg',
+    //   tum: 'img/gallery/12.jpg',
+    // //   cp: "photo 12"
+    // },
+    // { 
+    //   id: 13,
+    //   type: "Photos",
+    //   src: 'img/gallery/13.jpg',
+    //   tum: 'img/gallery/13.jpg',
+    // //   cp: "photo 13"
+    // },
+    // { 
+    //   id: 14,
+    //   type: "Photos",
+    //   src: 'img/gallery/14.jpg',
+    //   tum: 'img/gallery/14.jpg',
+    // //   cp: "photo 14"
+    // },
+    // { 
+    //   id: 15,
+    //   type: "Photoshop",
+    //   src: 'img/gallery/15.jpg',
+    //   tum: 'img/gallery/15.jpg',
+    // //   cp: "photo 15"
+    // },
+    // { 
+    //   id: 16,
+    //   type: "Photos",
+    //   src: 'img/gallery/16.jpeg',
+    //   tum: 'img/gallery/16.jpeg',
+    // //   cp: "photo 16"
+    // },
+    // { 
+    //   id: 17,
+    //   type: "Photos",
+    //   src: 'img/gallery/17.jpeg',
+    //   tum: 'img/gallery/17.jpeg',
+    // //   cp: "photo 17"
+    // },
+    // { 
+    //   id: 18,
+    //   type: "Photos",
+    //   src: 'img/gallery/18.jpeg',
+    //   tum: 'img/gallery/18.jpeg',
+    // //   cp: "photo 18"
+    // },
+    // { 
+    //   id: 19,
+    //   type: "Photoshop",
+    //   src: 'img/gallery/19.jpg',
+    //   tum: 'img/gallery/19.jpg',
+    // //   cp: "photo 19"
+    // },
+    // { 
+    //   id: 20,
+    //   type: "Photoshop",
+    //   src: 'img/gallery/20.jpeg',
+    //   tum: 'img/gallery/20.jpeg',
+    // //   cp: "photo 20"
+    // },
+    // { 
+    //   id: 21,
+    //   type: "Photoshop",
+    //   src: 'img/gallery/21.jpg',
+    //   tum: 'img/gallery/21.jpg',
+    // //   cp: "photo 21"
+    // },
+    // { 
+    //   id: 22,
+    //   type: "Photoshop",
+    //   src: 'img/gallery/22.jpg',
+    //   tum: 'img/gallery/22.jpg',
+    // //   cp: "photo 22"
+    // }
+  ];
